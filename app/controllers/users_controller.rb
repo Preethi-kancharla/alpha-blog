bb64078fbcb359b3bb829cf9be0eb926e1da7367
Preethi-kancharla <preethi.kancharla@freshworks.com>
2021-09-01 18:35:58 +0530

    added admin user and changed alert stylings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    before_action:set_user , only: [:show, :edit, :update]
-
+    before_action:set_user , only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy] 
 
     def new
         @user = User.new
@@ -41,6 +42,16 @@ class UsersController < ApplicationController
         
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == curent_user
+        flash[:notice] = "Account and all associated articles are successfully deleted"
+        redirect_to articles_path
+
+    end
+
+
+
     private
 
         def user_params
@@ -53,6 +64,13 @@ class UsersController < ApplicationController
                 @articles = @user.articles
             rescue ActiveRecord::RecordNotFound
                 render :json => "404 page not found"
+            end
+        end
+
+        def require_same_user
+            if current_user != @user && !current_user.admin?
+                flash[:alert] = "You can only edit or delete your own account"
+                redirect_to @user
             end
         end
 
