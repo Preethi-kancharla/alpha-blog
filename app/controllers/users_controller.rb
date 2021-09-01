ad8e26a1f288dccea1ef70a0a05220f96b986659
Preethi-kancharla <preethi.kancharla@freshworks.com>
2021-09-01 16:14:13 +0530

    added restrictions to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action:set_user , only: [:show, :edit, :update]
+
 
     def new
         @user = User.new
@@ -9,17 +11,17 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
-        @articles = @user.articles
+        
     end
 
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
             redirect_to articles_path
         else
@@ -29,19 +31,29 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find(params[:id])
+
         if @user.update(user_params)
             flash[:notice] = "Your account is successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
+        
     end
 
     private
 
         def user_params
             params.require(:user).permit(:username, :email, :password)
+        end
+
+        def set_user
+            begin
+                @user = User.find(params[:id])
+                @articles = @user.articles
+            rescue ActiveRecord::RecordNotFound
+                render :json => "404 page not found"
+            end
         end
 
 end
